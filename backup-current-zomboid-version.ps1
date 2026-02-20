84711d256041438505e6d9a27656fb2ac8cb022a
Connie <a.constanze.friedrichs@gmail.com>
2026-02-20 13:10:01 +0100

    backup script
    powershell script for backing up the current copy of the game

---
+++ backup-current-zomboid-version.ps1
@@ -0,0 +1,38 @@
+# -----------------------------------------------------------------------------
+# Change these two variables to __your__ Steam installation path
+# and the desired backup directory
+# -----------------------------------------------------------------------------
+$steamDir  = "G:\SteamLibrary\steamapps\"
+$backupDir = "E:\ZomboidBackup\"
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+$backupUserProfileDir	= $backupDir + "userprofile\"
+$backupGameDir			= $backupDir + "game\"
+$gameDir				= $steamDir + "common\ProjectZomboid"
+$workshopModsDir		= $steamDir + "workshop\content\108600\"
+$userProfileDir			= [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) + "\Zomboid\*"
+
+
+Write-Host "backing up game files"
+if (!(Test-Path -Path $backupGameDir)) {
+	[void](New-Item -Path $backupGameDir -ItemType Directory)
+}
+Copy-Item -Path $gameDir -Destination $backupGameDir -Recurse -Force
+
+
+Write-Host "backing up userprofile directory"
+if (!(Test-Path -Path $backupUserProfileDir)) {
+	[void](New-Item -Path $backupUserProfileDir -ItemType Directory)
+}
+Copy-Item -Path $userProfileDir -Destination $backupUserProfileDir -Recurse -Force
+
+
+Write-Host "copying mods to backed-up userprofile directory"
+$workshopMods     = Get-ChildItem $workshopModsDir -Name
+foreach ($modDir in $workshopMods) {
+	$sourcePath = $workshopModsDir + $modDir + "\mods\*"
+	$targetPath = $backupUserProfileDir + "mods\"
+	
+	Copy-Item -Path $sourcePath -Destination $targetPath -Recurse -Force
+}
