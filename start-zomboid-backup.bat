64aeccb8cebb8a229af53ca42897969fcc49f8f0
Connie <a.constanze.friedrichs@gmail.com>
2026-04-27 17:41:52 +0200

    Update start-zomboid-backup.bat
    removed the need for the directory junction; added startup-parameter

--- start-zomboid-backup.bat
+++ start-zomboid-backup.bat
@@ -6,7 +6,4 @@ SET backup_dir=C:\ZomboidBackup
  
 rem -----------------------------------------------------------------------------
 rem -----------------------------------------------------------------------------
-if exist "%USERPROFILE%\Zomboid" rmdir /s /q "%USERPROFILE%\Zomboid"
-mklink /j "%USERPROFILE%\Zomboid" "%backup_dir%\userprofile" 
- 
-start /d "%backup_dir%\game\" ProjectZomboid64.bat
+start /d "%backup_dir%\game\" ProjectZomboid64.bat "-cachedir=%backup_dir%\userprofile"
